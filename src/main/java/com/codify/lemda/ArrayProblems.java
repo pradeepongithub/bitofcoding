0bceec5db78aae0e911ed62bd11a53b16154be4a
Pradeep Yadav <rakuten.pradeep@gmail.com>
2019-01-28 15:33:20 +0530

    find pair sum

--- src/main/java/com/codify/lemda/ArrayProblems.java
+++ src/main/java/com/codify/lemda/ArrayProblems.java
@@ -1,5 +1,9 @@
 package com.codify.lemda;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class ArrayProblems {
 
   // Find last index of an element in a sorted array with duplicates
@@ -19,6 +23,13 @@ public static void main(String[] args) {
     System.out
         .println("Binary search complexity log(n) find first occurance of element found at index : "
             + findFirstIndexOfElementBinarySearch(10, array));
+    int[] arr = {2, 5, 9, 1, 5, 1, 8, 2, 8, 9, 5};
+    findNumberOccursOddTimes(arr);
+    findNumberOccursOddTimesHash(arr);
+
+    findPairWithGivenSum(6, arr);
+    findAllPairsForGivenSum(6, arr);
+
 }
 
   private static int findLastIndexOfElement(int ele, int... arr) {
@@ -67,5 +78,76 @@ public static void main(String[] args) {
     }
     return result;
   }
+
+  private static void findNumberOccursOddTimes(int... arr) {
+
+    int num = arr[0];
+
+    for (int i = 1; i < arr.length; i++) {
+      num = num ^ arr[i];
+
+    }
+
+    System.out.println("Number is : " + num);
+
+  }
+
+  private static void findNumberOccursOddTimesHash(int... arr) {
+
+    Map<Integer, Integer> map = new HashMap<>();
+    int number = 0, occurance = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      if (map.containsKey(arr[i])) {
+        map.put(arr[i], map.get(arr[i]) + 1);
+      } else
+      map.put(arr[i], 1);
+    }
+
+    for (Entry<Integer, Integer> m : map.entrySet()) {
+      if (m.getValue() % 2 != 0) {
+        number = m.getKey();
+        occurance = m.getValue();
+        break;
+      }
+    }
+
+    System.out.println("Hash Algo Number is : " + number + " occurs : " + occurance + " times");
+  }
+
+  private static void findPairWithGivenSum(int sum, int... arr) {
+
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < arr.length; i++) {
+      if (map.containsKey(sum - arr[i])) {
+        System.out.println("Pair found at index " + map.get(sum - arr[i]) + " and " + i);
+        return;
+      }
+      map.put(arr[i], i);
+    }
+
+    System.out.println("Pair not found");
+  }
+
+  private static void findAllPairsForGivenSum(int sum, int... arr) {
+
+    Map<Integer, Integer> map = new HashMap<>();
+
+    StringBuilder strBuilder = new StringBuilder();
+
+    for (int i = 0; i < arr.length; i++) {
+      if (map.containsKey(sum - arr[i])) {
+        strBuilder.append(" (").append(map.get(sum - arr[i])).append(", ").append(i).append(") ");
+      }
+      map.put(arr[i], i);
+
+    }
+
+    System.out.println("Pair for a given sum are : " + strBuilder);
+
+  }
+
+
 
 }
